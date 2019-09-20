d214d88a444fca423cad0bd323206c5e9b4e65d8
249786001@qq.com <249786001@qq.com>
2019-09-20 11:17:49 +0800

    first commit

---
+++ LZSearchController/DemoViewController.h
@@ -0,0 +1,17 @@
+//
+//  DemoViewController.h
+//  LZSearchController
+//
+//  Created by mkrq-yh on 2019/9/17.
+//  Copyright © 2019 Artup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DemoViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ LZSearchController/DemoViewController.m
@@ -0,0 +1,33 @@
+//
+//  DemoViewController.m
+//  LZSearchController
+//
+//  Created by mkrq-yh on 2019/9/17.
+//  Copyright © 2019 Artup. All rights reserved.
+//
+
+#import "DemoViewController.h"
+
+@interface DemoViewController ()
+
+@end
+
+@implementation DemoViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.view.backgroundColor = [UIColor greenColor];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
